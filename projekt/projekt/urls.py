41b2f0e66674e837f2606ad8bc4e4252ef3f1304
Adam Kulhavý <58812061+bananarulez@users.noreply.github.com>
2020-05-28 12:45:49 +0200

    zmena

--- projekt/projekt/urls.py
+++ projekt/projekt/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('entries.urls')),
-    url(r'^data/$',include('entries.urls')),
-    url(r'^login/$', include('entries.urls')),
+    url(r'^data/',include('entries.urls')),
+    url(r'^login/', include('entries.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 
