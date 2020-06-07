3e6f756e769f50abb644681e7be6a2378ed7bf2d
Michal Zíma <michal.zima@skaut.cz>
2020-06-07 11:37:52 +0200

    přidáno Add, delete, edit(jen formulář)

--- projekt/entries/urls.py
+++ projekt/entries/urls.py
@@ -4,11 +4,14 @@ from . import views
 urlpatterns = [
     path(r'', views.index, name='home'),
     path(r'^add/$', views.add, name='add'),
+    path(r'add_data/', views.add_data, name='add_data'),
     path(r'^delete/$', views.delete, name='delete'),
     path(r'^login/$', views.login, name='login'),
     path(r'^data/$', views.data, name='data'),
     path(r'delete_autor/<int:id>', views.delete_autor, name='delete_autor'),
-    path(r'domu', views.domu, name='domu'), 
+    path(r'edit/<int:id>', views.edit, name='edit'),    
+    path(r'edit_data/<int:id>', views.edit_data, name='edit_data'),
+    path(r'domu', views.domu, name='domu'),
 
 
 
--- projekt/entries/views.py
+++ projekt/entries/views.py
@@ -3,9 +3,33 @@ from .models import Iautorsky
 
 def index(request):
     return render(request, 'entries/index.html')
+def edit(request,id):
+    return render(request, 'entries/edit.html') 
+def edit_data(request,id):
+    jmeno=request.POST.get("jmeno_e")
+    vyzkum=request.POST.get("vyzkum_e")
+    teren=request.POST.get("teren_e")
+    technicky=request.POST.get("technicky_e")
+    kreslil=request.POST.get("kreslil_e")
+    fotografoval=request.POST.get("fotografoval_e")
+    cislo_nove=request.POST.get("cislo_nove_e")
+    obj=Iautorsky(jmeno=jmeno,vyzkum=vyzkum,teren=teren,technicky=technicky,kreslil=kreslil,fotografoval=fotografoval,cislo_nove=cislo_nove)
+    obj.save()
+    return render(request, 'entries/index.html')
 
 def add(request):
     return render(request, 'entries/add.html')
+def add_data(request):
+    jmeno=request.POST.get("jmeno_n")
+    vyzkum=request.POST.get("vyzkum_n")
+    teren=request.POST.get("teren_n")
+    technicky=request.POST.get("technicky_n")
+    kreslil=request.POST.get("kreslil_n")
+    fotografoval=request.POST.get("fotografoval_n")
+    cislo_nove=request.POST.get("cislo_nove_n")
+    obj=Iautorsky(jmeno=jmeno,vyzkum=vyzkum,teren=teren,technicky=technicky,kreslil=kreslil,fotografoval=fotografoval,cislo_nove=cislo_nove)
+    obj.save()
+    return render(request, 'entries/index.html')
 
 def data(request):
     entries = Iautorsky.objects.all()
@@ -24,19 +48,10 @@ def delete(request):
 
     return render(request, 'entries/delete.html', context)
 
-
-
-
 def delete_autor(request, id):
     lide = Iautorsky.objects.get(id=id)
     lide.delete()
     return render(request, 'entries/report_smazani.html')
-
-
-
-
-
-
 
 def domu(request):
 
